c368a9a5c91d2ce319794e04b2ab6edfc991947a
mindofsimon <simone5.mariani@mail.polimi.it>
2021-12-11 16:30:35 +0100

    little update on csv creation

--- create_interpolated_videos/create_csv_speednet.py
+++ create_interpolated_videos/create_csv_speednet.py
@@ -1,11 +1,10 @@
 """
 Save video informations in csv file.
-Set the speed manipulation parameter of the videos, along with the roots in which original and manipulated videos are stored.
+Set the speed manipulation parameter of the videos, along with the roots in which original videos are stored.
 
 Each line of the csv contains:
     video path, video label (original = 0, manipulated = 1), video smp (if video is original is set to 1).
-    train videos will contain just original videos (sped-up versions will be created during training).
-    test/validation videos will contain both a mix of original and sped-up videos.
+    train/test/validation videos will contain just original videos (sped-up versions will be created during training).
 """
 
 import csv
@@ -17,12 +16,10 @@ smp = 2
 
 # TEST
 originals_root = '/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/test/originals/'
-manipulated_root = '/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/test/2x/'
 with open('/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/test/test.csv', mode='w', newline="") as videos:
     videos_writer = csv.writer(videos, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for v in os.listdir(originals_root):
         videos_writer.writerow([originals_root + v, "0", "1"])
-        videos_writer.writerow([manipulated_root + v, "1", smp])
 
 # TRAIN
 originals_root = '/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/train/originals/'
@@ -33,9 +30,7 @@ with open('/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_
 
 # VALIDATION
 originals_root = '/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/validation/originals/'
-manipulated_root = '/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/validation/2x/'
 with open('/nas/home/smariani/video_interpolation/datasets/kinetics400/kinetics_videos/validation/validation.csv', mode='w', newline="") as videos:
     videos_writer = csv.writer(videos, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for v in os.listdir(originals_root):
         videos_writer.writerow([originals_root + v, "0", "1"])
-        videos_writer.writerow([manipulated_root + v, "1", smp])
