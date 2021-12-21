753ae802391f0fe0ee34098b3cdd43c0f5fc93cb
mindofsimon <simone5.mariani@mail.polimi.it>
2021-12-21 15:36:12 +0100

    little fix on test/train balance

--- create_interpolated_videos/create_csv_speednet.py
+++ create_interpolated_videos/create_csv_speednet.py
@@ -29,7 +29,7 @@ with open('/nas/home/smariani/video_interpolation/datasets/kinetics400/test.csv'
 # TRAIN
 originals_root = '/nas/home/pbestagini/kinetics/k400/train/'
 video_list = [v for v in os.listdir(originals_root) if v.endswith('.mp4')]
-video_list = video_list[0:10000]
+video_list = video_list[0:9000]
 with open('/nas/home/smariani/video_interpolation/datasets/kinetics400/train.csv', mode='w', newline="") as videos:
     videos_writer = csv.writer(videos, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for v in video_list:
